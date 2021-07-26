395833e36d54a92de016fee0d65056e0bfd2587b
flashfinisher7 <mvsanilkumar07@gmail.com>
2021-07-26 13:23:18 +0530

    storing result in array

--- SortingArithmatic.sh
+++ SortingArithmatic.sh
@@ -3,18 +3,30 @@ echo "Welcome to sorting arithmatic problem statement"
 read -p "Enter value for a: " a
 read -p "Enter value for b: " b
 read -p "Enter value for c: " c
+
 x=$(($a + $b * $c))
 echo "output of equation " $x
+
 parttimehrs=8
 y=$[(($a * $b + $c))*$(($parttimehrs))]
 echo "work in hours" $y
+
 z=$(($c + $a/$b))
 echo "output of equation" $z
+
 r=$(($a%$b+$c))
 echo "output of equation " $r
+
 declare -A  dictionary
 dictionary[key]=$x
 dictionary[key1]=$y
 dictionary[key2]=$z
 dictionary[key3]=$r
 echo ${dictionary[@]}
+
+count=0;
+for i in ${dictionary[@]}
+do
+	resultArray[((count++))]=$i;
+done
+
